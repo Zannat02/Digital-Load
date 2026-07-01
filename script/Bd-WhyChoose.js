fa6f8c246e8e939422ffffcd92f859bf9e1ddb59
zannat02 <shawon4166@gmail.com>
2026-07-01 16:17:54 +0600

    Move business development Css files to css folder

--- script/Bd-WhyChoose.js
+++ script/Bd-WhyChoose.js
@@ -10,15 +10,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
         button.addEventListener("click", () => {
 
-            // যদি এটিই already open থাকে
+       
             const isActive = item.classList.contains("active");
 
-            // সব close
+          
             faqItems.forEach((faq) => {
                 faq.classList.remove("active");
             });
 
-            // যদি আগে close থাকে তাহলে open হবে
+        
             if (!isActive) {
                 item.classList.add("active");
             }
