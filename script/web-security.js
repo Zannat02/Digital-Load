c89233021a38eeea333b559767c7c9d223858579
zannat02 <shawon4166@gmail.com>
2026-07-18 23:45:16 +0600

    fixed mobile app hero section responsive, service page card responsive, SWOT  and all of those pages hero section button responsive fixed

--- script/web-security.js
+++ script/web-security.js
@@ -1,28 +1,4 @@
-// Scroll Reveal
 
-// const securityElements = document.querySelectorAll('.securityReveal');
-
-// const securityObserver = new IntersectionObserver((entries) => {
-
-//     entries.forEach((entry) => {
-
-//         if (entry.isIntersecting) {
-
-//             entry.target.classList.add('active');
-
-//         }
-
-//     });
-
-// }, {
-//     threshold: 0.2
-// });
-
-// securityElements.forEach((element) => {
-
-//     securityObserver.observe(element);
-
-// });
 
 
 const securityElements = document.querySelectorAll('.securityReveal');
